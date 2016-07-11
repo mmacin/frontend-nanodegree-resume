4f8da11f153941c52e3bb27310ec2b2a551989e0
maciolem <marcin.maciolek@roche.com>
2016-07-11 13:54:20 +0200

    1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,65 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name": "Marcin M.",
+	"role": "Web Developer",
+	"contact": {
+		"mobile": "123456",
+		"email": "mmacin@wp.pl",
+		"github": "mmacin",
+		"twitter": "mmacinn",
+		"location": "Warsaw"
+	},
+	"welcomeMessage": "Hi there!",
+	"skills": ["awesomeness", "programming", "teaching", "JS"],
+	"bioPic": "https://d13yacurqjgara.cloudfront.net/users/2790/screenshots/1527123/magnus_moan_final_1x.png"
+}
+
+var education = {
+	"schools": [{
+		"name": "Elektronik",
+		"city": "Rzeszow",
+		"degree": "Technican",
+		"majors": ["CS"],
+		"dates": 2004,
+		"url": "http://onet.pl"
+	}, {
+		"name": "Wsiz",
+		"city": "Rzeszow",
+		"degree": "IT Specialist",
+		"majors": ["CS"],
+		"dates": 2008,
+		"url": "http://wp.pl"
+	}],
+	"onlineCourses": [{
+		"title": "JS Crash Course",
+		"school": "Udacity",
+		"dates": 2016,
+		"url": "Http://interia.pl"
+	}]
+}
+
+var work =  {
+ 	"jobs": [{
+ 		"employer": "New Balance",
+ 		"title": "IT Specialist",
+ 		"dates": "January 2323 - Future",
+ 		"description": "dasaasdij a'sjd asopjd' asldk aopskf asdlfk asldkf asldkf asldkf"
+ 	}, {
+ 		"employer": "Roche",
+ 		"title": "IT Specialist",
+ 		"dates": "January 2323 - Future",
+ 		"description": "dasaasdij a'sjd asopjd' asldk aopskf asdlfk asldkf asldkf asldkfdasaasdij a'sjd asopjd' asldk aopskf asdlfk asldkf asldkf asldkfdasaasdij a'sjd asopjd' asldk aopskf asdlfk asldkf asldkf asldkf"
+ 	}]
+ }
+
+var projects = {
+	"projects": [{
+		"title": "Simple pro",
+		"dates": "2016",
+		"description": "sldk aopskf asdlfk asldkf asldkf asldkfdasaasdij a'sjd asopjd' asldk aopskf asdlfk asldkf asldkf asldkf",
+		"images": [
+			"https://upload.wikimedia.org/wikipedia/commons/f/f4/Luxury_lamp_IMG_4333.JPG",
+			"https://upload.wikimedia.org/wikipedia/commons/f/f4/Luxury_lamp_IMG_4333.JPG"
+		]
+
+	}]
+}
